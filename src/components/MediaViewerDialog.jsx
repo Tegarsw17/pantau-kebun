a542f5b7564a5bc7fd802a2ace694a4cc03589ff
Tegarsw17 <tegarsatriyawiguna99@gmail.com>
2026-05-16 15:40:42 +0700

    revert(monitoring): restore scrollable media viewer layout

--- src/components/MediaViewerDialog.jsx
+++ src/components/MediaViewerDialog.jsx
@@ -124,7 +124,7 @@ export function MediaViewerDialog({
 
         <div className="media-viewer__body">
           <button
-            className="media-viewer__nav media-viewer__nav--prev"
+            className="media-viewer__nav"
             disabled={isFirstAsset}
             onClick={() => onNavigate(-1)}
             type="button"
@@ -137,7 +137,7 @@ export function MediaViewerDialog({
           </div>
 
           <button
-            className="media-viewer__nav media-viewer__nav--next"
+            className="media-viewer__nav"
             disabled={isLastAsset}
             onClick={() => onNavigate(1)}
             type="button"
