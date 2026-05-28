571905bd990f40330836b5fec1eafcc284bbd4ae
Tegarsw17 <tegarsatriyawiguna99@gmail.com>
2026-05-28 13:11:44 +0700

    Revert "style(inventory): restore admin card button composition"
    This reverts commit 2983e894c23b3e463ee4891d0c08092346bdf3a4.

--- src/components/InventoryWorkspace.jsx
+++ src/components/InventoryWorkspace.jsx
@@ -187,6 +187,13 @@ function InventoryItemCard({ item, onEditRequest, onHistoryRequest, onMutationRe
         <div className="inventory-card__actions">
           <button
             className="inventory-secondary-button"
+            onClick={() => onHistoryRequest?.(item)}
+            type="button"
+          >
+            History
+          </button>
+          <button
+            className="inventory-secondary-button"
             onClick={() => onEditRequest?.(item)}
             type="button"
           >
@@ -200,17 +207,15 @@ function InventoryItemCard({ item, onEditRequest, onHistoryRequest, onMutationRe
             Mutasi Stok
           </button>
         </div>
-      ) : null}
-
-      <button
-        className={`inventory-card-history-button ${
-          isAdmin ? "inventory-card-history-button--admin" : ""
-        }`}
-        onClick={() => onHistoryRequest?.(item)}
-        type="button"
-      >
-        View History
-      </button>
+      ) : (
+        <button
+          className="inventory-card-history-button"
+          onClick={() => onHistoryRequest?.(item)}
+          type="button"
+        >
+          View History
+        </button>
+      )}
     </article>
   );
 }
